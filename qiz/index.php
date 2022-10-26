b0645a34dbb4691adb293d84c6b03687f6a79a95
SzYmoonJ <kaliozajankowski@gmail.com>
2022-10-26 16:05:28 +0200

    Podstawy storny i baza z danymi

---
+++ qiz/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>QUIZ</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
+            $con = new mysqli("127.0.0.1", "root","","qiz");
+
+        ?>
+    </body>
+</html>
